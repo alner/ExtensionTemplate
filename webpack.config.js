b69a9a163769bcc21ddbc1fd0d6d2965f1725b85
Kevin McGovern <kmcg82004@gmail.com>
2017-08-15 10:53:42 -0400

    webpack and wds deps; header settings
    Updated webpack and wds dependencies to alleviate 404 error
    Changed dev server header settings to fix issue with CORS

--- webpack.config.js
+++ webpack.config.js
@@ -127,7 +127,9 @@ module.exports = (env = {}) => {
             historyApiFallback: true,
             hotOnly: true,
             headers: {
-                "Access-Control-Allow-Origin": "*",
+                // "Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Origin": "http://localhost:4848",
+				"Access-Control-Allow-Credentials": "true",
                 "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
                 "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
             },
